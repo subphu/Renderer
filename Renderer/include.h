0323091879da0c108061f445a67e16648580fea5
Subroto <subrotoph@gmail.com>
2023-03-26 00:12:32 +0900

    Add macro

---
+++ Renderer/include.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#define GLM_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+#include <glm/gtx/hash.hpp>
+
+#include <stdexcept>
+#include <string>
+#include <array>
+#include <vector>
+#include <math.h>
+
+#include <macro.h>
